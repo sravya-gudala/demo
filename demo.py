7a61917907cdbe9dc82e3ce6f45274dbdf49503e
sravya-gudala <147653473+sravya-gudala@users.noreply.github.com>
2024-01-08 12:15:40 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,10 +5,10 @@ st.header("Presenting...")
 col1,col2 = st.columns(2)
 with col1:
   st.subheader("Girl")
-  st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.shutterstock.com%2Fsearch%2Fcute-anime-chibi-little-girl-cartoon&psig=AOvVaw3MJbPLSqgf14GIim0X5aVq&ust=1704782388363000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCrprqXzYMDFQAAAAAdAAAAABAD", caption="Persian Cat", width=300, use_column_width=True)
+  st.image("https://i.pinimg.com/736x/10/82/d7/1082d763e8eb696484449d20ae7578b9.jpg", caption="Gurl", width=300, use_column_width=True)
   st.write("Girls are cute")
   
 with col2:
   st.subheader("Boy")
-  st.image("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F843932417677448727%2F&psig=AOvVaw01OmLvUGSSnZj5beL1RN-C&ust=1704782486855000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDb9O6XzYMDFQAAAAAdAAAAABAD", caption="Ragdoll Cat", width=300, use_column_width=True)
+  st.image("https://i.pinimg.com/736x/10/82/d7/1082d763e8eb696484449d20ae7578b9.jpg", caption="Boi", width=300, use_column_width=True)
   st.write("Boys are Potatoes")
