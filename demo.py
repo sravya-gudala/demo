cb4793a2929f232dfcafebac81968bebf1446c4a
sravya-gudala <147653473+sravya-gudala@users.noreply.github.com>
2024-01-08 12:36:09 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
 import streamlit as st
-st.set_page_config(page_title='Dumb')
-st.header("Presenting...")
+st.set_page_config(page_title='Magic')
+st.header("Hogwarts")
 
 col1,col2 = st.columns(2)
 with col1:
-  st.subheader("Girl")
-  st.image("https://www.shutterstock.com/image-vector/cute-anime-chibi-little-girl-600nw-2315416261.jpg", caption="Gurl", width=300, use_column_width=True)
-  st.write("Girls are cute")
+  st.subheader("Gryffindor")
+  st.image("https://i.pinimg.com/originals/81/7d/36/817d3670dbdf4a263893c476bcda0ca3.jpg", caption="Harry Potter", width=300, use_column_width=True)
+  st.write("Shut up, Malfoy!")
   
 with col2:
-  st.subheader("Boy")
-  st.image("https://i.pinimg.com/736x/10/82/d7/1082d763e8eb696484449d20ae7578b9.jpg", caption="Boi", width=300, use_column_width=True)
-  st.write("Boys are Potatoes")
+  st.subheader("Slytherin")
+  st.image("https://img.freepik.com/premium-vector/vector-chibi-draco-malfoy_969040-25.jpg", caption="Draco Malfoy", width=300, use_column_width=True)
+  st.write("ooh Dementor! Dementor!")
