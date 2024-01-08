e17f329c1dfcb09ac72c9e5d7180a35e86e4025e
sravya-gudala <147653473+sravya-gudala@users.noreply.github.com>
2024-01-08 12:16:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ st.header("Presenting...")
 col1,col2 = st.columns(2)
 with col1:
   st.subheader("Girl")
-  st.image("https://i.pinimg.com/736x/10/82/d7/1082d763e8eb696484449d20ae7578b9.jpg", caption="Gurl", width=300, use_column_width=True)
+  st.image("https://www.shutterstock.com/image-vector/cute-anime-chibi-little-girl-600nw-2315416261.jpg", caption="Gurl", width=300, use_column_width=True)
   st.write("Girls are cute")
   
 with col2:
