2e70726c843fded7d463e97b6960c3aed85c7fd7
sravya-gudala <147653473+sravya-gudala@users.noreply.github.com>
2024-01-08 14:01:47 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ st.header("Hogwarts")
 col1,col2 = st.columns(2)
 with col1:
   st.subheader("Gryffindor")
-  st.image("https://i.pinimg.com/originals/81/7d/36/817d3670dbdf4a263893c476bcda0ca3.jpg", caption="Harry Potter", width=300, use_column_width=True)
+  st.image("https://pbs.twimg.com/profile_images/1173160762/HarryPotterL_468x456_400x400.jpg", caption="Harry Potter", width=300, use_column_width=True)
   st.write("Shut up, Malfoy!")
   
 with col2:
